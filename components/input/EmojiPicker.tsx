d94cfd49f991208daafdb78412969698a3613e33
david emioma <davidemiomauche@gmail.com>
2023-11-09 17:29:57 +0000

    Fixed emoji issues

--- components/input/EmojiPicker.tsx
+++ components/input/EmojiPicker.tsx
@@ -1,45 +1,39 @@
 "use client";
 
 import React from "react";
-import dynamic from "next/dynamic";
-import data from "@emoji-mart/data";
 import { Smile } from "lucide-react";
-import Picker from "@emoji-mart/react";
+import EmojiPicker from "emoji-picker-react";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
 
-// const Picker = dynamic(
-//   async () => (await import("@emoji-mart/react")).default,
-//   { ssr: false }
-// );
-
 interface Props {
   onChange: (value: string) => void;
 }
 
-const EmojiPicker = ({ onChange }: Props) => {
+const EmojiPickerComponent = ({ onChange }: Props) => {
   return (
     <Popover>
-      <PopoverTrigger>
-        <Smile className="text-sky-500" size={20} />
+      <PopoverTrigger asChild>
+        <Smile className="text-sky-500 cursor-pointer" size={20} />
       </PopoverTrigger>
 
       <PopoverContent
         className="bg-transparent border-none shadow-none drop-shadow-none mb-16"
-        side="right"
-        sideOffset={-30}
+        // side="right"
+        // sideOffset={-30}
       >
-        <Picker
-          data={data}
+        <EmojiPicker
+          height={350}
+          // @ts-ignore
           theme="dark"
-          onEmojiSelect={(emoji: any) => onChange(emoji.native)}
+          onEmojiClick={(data) => onChange(data.emoji)}
         />
       </PopoverContent>
     </Popover>
   );
 };
 
-export default EmojiPicker;
+export default EmojiPickerComponent;
